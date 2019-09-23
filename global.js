7ce1bc2ec1d004a27f83fd1b2f24cf6154935990
mdubourg <mdubourg@akema.fr>
2019-09-23 23:01:33 +0200

    events handler on searchbar input and cross click

--- global.js
+++ global.js
@@ -1,19 +1,45 @@
-const handleSearchbarFocus = event => {
-  console.log("coucou");
-  document.getElementById("searchbar-cross").classList.remove("hidden");
+/* ======== CONSTANTS ======= */
+
+const GIPHY_SEARCH_API_ENDPOINT = "https://api.giphy.com/v1/gifs/search";
+const GIPHY_DEV_API_KEY = "hYs1n6z0WOkt24K6w7ttx18gnRLh8Gks";
+
+/* ======== EVENT HANDLERS ======= */
+
+const handleSearchbarInput = (event, searchbarCrossHTMLElement) => {
+  if (event.target.value.length === 0)
+    searchbarCrossHTMLElement.classList.add("hidden");
+  else searchbarCrossHTMLElement.classList.remove("hidden");
+
+  if (event.target.value.length > 0) {
+    fetch(
+      `${GIPHY_SEARCH_API_ENDPOINT}?api_key=${GIPHY_DEV_API_KEY}&q=${event.target.value}`,
+      response => {},
+      error => {}
+    );
+  }
+
+  // TODO IMPLEMENT HREF UPDATE
 };
 
-const handleSearchbarBlur = event => {
-  document.getElementById("searchbar-cross").classList.add("hidden");
+const handleSearchbarCrossClick = (event, searchbarInputHTMLElement) => {
+  searchbarInputHTMLElement.value = "";
+  event.target.classList.add("hidden");
 };
 
-document.onload = () => {
-  // display cross icon on searchbar input focus
-  document
-    .getElementById("searchbar-input")
-    .addEventListener("focus", handleSearchbarFocus);
-  // hide cross icon on searchbar input focus
-  document
-    .getElementById("searchbar-input")
-    .addEventListener("blur", handleSearchbarBlur);
+window.onload = () => {
+  const searchbarInput = document.getElementById("searchbar-input");
+  const searchbarCross = document.getElementById("searchbar-cross");
+
+  const gifsWrapper = document.getElementById("gifs-wrapper");
+
+  /* ======== SEARCHBAR ELISTENERS ======= */
+
+  // loads GIFs, updates browser href and manage cross icon display
+  searchbarInput.addEventListener("input", event =>
+    handleSearchbarInput(event, searchbarCross)
+  );
+
+  searchbarCross.addEventListener("click", event =>
+    handleSearchbarCrossClick(event, searchbarInput)
+  );
 };
