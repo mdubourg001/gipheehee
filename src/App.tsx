455e300497ed17f64b59c221d75e82a80d0e3545
Maxime Dubourg <maxime.dubourg@ekino.com>
2019-09-26 17:58:33 +0200

    feat: set up of the cra skeleton + tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../node_modules/tailwindcss/dist/tailwind.min.css";
+
+const App: React.FC = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
